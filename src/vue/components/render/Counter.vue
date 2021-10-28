d90868d5f02e67bc6fb16208460b2cab02788b7b
trapzpro <43911195+trapzpro@users.noreply.github.com>
2021-10-28 11:42:28 -0400

    start

---
+++ src/vue/components/render/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button @click="increment">increment</button>
+    <button @click="decrement">decrement</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Counter",
+   props: {
+    products: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+     howMany() {
+       return this.products.count;
+    },
+    decrement() {
+      this.count--;
+    },
+  },
+};
+</script>
