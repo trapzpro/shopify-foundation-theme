825c45bd69bb81441d194829ca687709e7e056b0
trapzpro <43911195+trapzpro@users.noreply.github.com>
2021-10-28 20:26:22 -0400

    weird slider working

---
+++ src/vue/components/renderless/Slideshow.vue
@@ -0,0 +1,37 @@
+<script>
+
+export default {
+  props: {
+   
+  },
+  setup (props, { slots }) {
+
+    const nextSlide =  () =>  {
+        let activeSlide = document.querySelector('.slide.translate-x-0');
+        activeSlide.classList.remove('translate-x-0');
+        activeSlide.classList.add('-translate-x-full');
+
+        let nextSlide = activeSlide.nextElementSibling;
+        nextSlide.classList.remove('translate-x-full');
+        nextSlide.classList.add('translate-x-0');
+    }
+
+
+ const previousSlide =  () => {
+    let activeSlide = document.querySelector('.slide.translate-x-0');
+    activeSlide.classList.remove('translate-x-0');
+    activeSlide.classList.add('translate-x-full');
+
+    let previousSlide = activeSlide.previousElementSibling;
+    previousSlide.classList.remove('-translate-x-full');
+    previousSlide.classList.add('translate-x-0');
+}
+
+    return () => slots.default({
+      nextSlide,
+      previousSlide
+      
+    })
+  }
+}
+</script>
